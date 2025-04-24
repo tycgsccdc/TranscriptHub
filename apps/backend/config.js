1d33b116880d34f3823814f9378bdda4abf71515
andyjjrt <andyjjrt@gmail.com>
2025-04-24 15:42:57 +0800

    fix: Add port support for db and unify DB_HOST

--- apps/backend/config.js
+++ apps/backend/config.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const sql_config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
+  server: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '1433'),
   database: process.env.DB_NAME,
   options: {
     encrypt: false, // Enable encryption if needed
