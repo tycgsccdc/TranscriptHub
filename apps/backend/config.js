4511c8189180fa04d42006f954e9d68dd4b991a7
andyjjrt <andyjjrt@gmail.com>
2025-05-05 14:05:59 +0800

    fix: Rename `DB_HOST` to `DB_SERVER`

--- apps/backend/config.js
+++ apps/backend/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sql_config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_HOST || 'localhost',
+  server: process.env.DB_SERVER || 'localhost',
   port: parseInt(process.env.DB_PORT || '1433'),
   database: process.env.DB_NAME,
   options: {
